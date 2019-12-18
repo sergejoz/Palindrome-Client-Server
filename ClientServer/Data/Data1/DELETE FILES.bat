f5a9850b467877ee1096afd4d2b5dfbf5c77ca8b
Sergey Menshikov <jozmvm@gmail.com>
2019-12-18 08:56:59 +0500

    Added data.rar
    Перезалил свои тестовые данные архивом

--- ClientServer/Data/Data1/DELETE FILES.bat
+++
@@ -1,5 +0,0 @@
-echo Batch to delete file
-
-del "D:\STUDY\Shared\Projects\ClientServer\Data\Data1\*.txt" /s /f /q
-
-echo Done!
--- ClientServer/Data/Data2/DELETE FILES.bat
+++
@@ -1,5 +0,0 @@
-echo Batch to delete file
-
-del "D:\STUDY\Shared\Projects\ClientServer\Data\*.txt" /s /f /q
-
-echo Done!
